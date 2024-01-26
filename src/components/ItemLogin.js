b4e8394eea33e53f0ef3171262c60cab56dadd8d
Luan Henrique <luanhcosta03@gmail.com>
2024-01-25 23:53:30 -0300

    Insert login

--- src/components/ItemLogin.js
+++ src/components/ItemLogin.js
@@ -2,7 +2,7 @@ import Paper from "@mui/material/Paper";
 import { experimentalStyled as styled } from '@mui/material/styles';
 
 export const Item = styled(Paper)
-(({ theme }) => ({backgroundColor: "#07090E",
+(({ theme }) => ({backgroundColor: "#05060c",
 padding: theme.spacing(2),
 textAlign: 'center',
 color: theme.palette.text.secondary,
--- src/containers/login.js
+++ src/containers/login.js
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import { experimentalStyled as styled } from '@mui/material/styles';
+import Button from "@mui/material/Button";
 import identidade from "../images/identidade.jpg"
 import { useNavigate } from 'react-router-dom';
 import { RemoveScroll } from 'react-remove-scroll';
@@ -13,8 +14,10 @@ export default function Login(){
     const navigate = useNavigate();
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
-    console.log(email);
-    console.log(password);
+
+    const handleLogin = () => {
+        alert('username: ${emai} password: ${password}');
+    };
 
     return(
         <React.Fragment>
@@ -22,22 +25,23 @@ export default function Login(){
                 <Box sx={{ flexGrow: 1 }}>
                     <Grid container columns={{ xs: 4, sm: 8, md:12}}>
                         <Grid item md={6} sx={{
-                            position:'absolute',
                             right:'0',
                             top:'0',
                             width:'100%',
                             height:'100vh'}}>
-                            <Item container marginTop={27}>
+                            <Item>
                                     <img src={identidade} style={{width: '182px', 
                                     height: '183px', 
                                     WebkitMaskPositionX: '255PX', 
-                                    WebKitMaskPositionY: '110px' 
+                                    WebKitMaskPositionY: '110px',
+                                    marginTop: '137px' 
                                     }}/>
 
                                 <Typography
                                     variant="h6"
                                     gutterBottom sx={{ color: "#C2F6F7", 
-                                    fontsize: '32px'}}>
+                                    fontsize: '32px',
+                                    margin: '14px'}}>
                                         Username
                                 </Typography>
 
@@ -48,13 +52,15 @@ export default function Login(){
                                     sx={{ width:'326px',
                                         height:'46px',
                                         borderRadius:'12px',
-                                        backgroundColor: "#16203C"}}
+                                        backgroundColor: "#8eabc2",
+                                        margin:'1px'}}
                                     onChange={(e) => setEmail(e.target.value)}/>
 
                                 <Typography
                                     variant="h6"
                                     gutterBottom sx={{ color: "#C2F6F7", 
-                                    fontsize: '32px'}}>
+                                    fontsize: '32px',
+                                    margin:'14px'}}>
                                         Password
                                 </Typography>
 
@@ -65,12 +71,12 @@ export default function Login(){
                                     sx={{ width:'326px',
                                         height:'46px',
                                         borderRadius:'12px',
-                                        backgroundColor: "#16203C"}}
-                                    onChange={(e) => setPassword(e.target.value)}/>
+                                        backgroundColor: "#8eabc2",
+                                        margin:'1px'}}
+                                    onChange={(e) => setPassword(e.target.value)} />
                             </Item>
                         </Grid>
                         <Grid item md={6} sx={{
-                            position:'absolute',
                             left:'0',
                             top:'0',
                             width:'100%',
